5e5baedaea1a856de62aa0bcc0a954bc12745a71
Robert Foy <robert.foy@gmail.com>
2019-02-17 12:20:20 +1100

    Basic "Checkout" object

---
+++ src/main/kotlin/com/rjf/dius/shopping/Checkout.kt
@@ -0,0 +1,25 @@
+package com.rjf.dius.shopping
+
+import com.rjf.dius.shopping.model.Item
+import com.rjf.dius.shopping.model.PricingRule
+import com.rjf.dius.shopping.model.PricingRule.*
+import java.math.BigDecimal
+import java.math.BigDecimal.ZERO
+import java.math.RoundingMode.DOWN
+
+
+/**
+ * A checkout system for tracking a group of [items].
+ *
+ * @property pricingRules the pricing rules / discounts to apply to this checkout.
+ * @property items the items being checked out.
+ * @constructor Creates a new checkout.
+ */
+class Checkout(private val pricingRules: MutableList<PricingRule>) {
+    var items: MutableList<Item> = mutableListOf()
+
+    /**
+     * Adds [newItems] to the list of [items] for checkout.
+     */
+    fun scan(vararg newItems: Item) = items.addAll(newItems)
+}
